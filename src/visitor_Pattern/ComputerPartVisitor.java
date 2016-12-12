29fbf28918592cf24e6580d0bf90d6cb783edc89
Renan Thiago <renannojosa@gmail.com>
2016-12-12 18:25:28 -0300

    Update ComputerPartVisitor.java

--- src/visitor_Pattern/ComputerPartVisitor.java
+++ src/visitor_Pattern/ComputerPartVisitor.java
@@ -2,9 +2,11 @@ package visitor_Pattern;
 
 public interface ComputerPartVisitor {
 	public void visit(Memoria memoria);
+	public void visit(Computador computador);
+	public void visit(Gmemoria gram);
+	public void visit(Gpu gpu);
 	public void visit(Teclado teclado);
 	public void visit(Monitor monitor);
-	public void visit(Computador computador);
 	public void visit(Cpu amd);
-	public void visit(Gcard nVidea);
+		
 }
