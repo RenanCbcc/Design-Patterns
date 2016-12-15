3484e2813c5d469a3f88193d2193fb049f2ad53c
Renan Thiago <renannojosa@gmail.com>
2016-12-14 22:08:20 -0300

    Update DecoratorPatternDemo.java

--- src/decorator_Pattern/DecoratorPatternDemo.java
+++ src/decorator_Pattern/DecoratorPatternDemo.java
@@ -1,20 +1,25 @@
 package decorator_Pattern;
 
+import javax.swing.JOptionPane;
+
 public class DecoratorPatternDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Sorvete sorvete = new Sorvete_Pavê();
-		System.out.println(sorvete.fazer_Sorvete()); 
+		Sorvete sorvete = new Sorvete_Chocolate();
+		
+		JOptionPane.showMessageDialog(null,sorvete.fazer_Sorvete()); 
 		
 		Sorvete sorvete_com_Cobertura = new Cobertura_Chocolate(sorvete);
 		
-		System.out.println(sorvete_com_Cobertura.fazer_Sorvete()); 
-		System.out.println(sorvete_com_Cobertura.preço_Sorvete());
+		JOptionPane.showMessageDialog(null,sorvete_com_Cobertura.fazer_Sorvete()+"\n"
+				+sorvete_com_Cobertura.preÃ§o_Sorvete()); 
 		
-		Sorvete copinho = new Sovete_Taça(sorvete);
-		System.out.println(copinho.preço_Sorvete());
+		
+		Sorvete copinho = new Sovete_TaÃ§a(sorvete);
+		JOptionPane.showMessageDialog(null,copinho.fazer_Sorvete()+"\n"+
+				copinho.preÃ§o_Sorvete());
 		
 	}
 
